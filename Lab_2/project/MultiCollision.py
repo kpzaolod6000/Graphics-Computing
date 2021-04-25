88c1f7dd1854d94ebbe45dff8ea959c31c1e4205
kpzaolod6000 <kpzaolod6000@gmail.com>
2021-04-24 19:07:14 -0500

    update the set_initial-position and callback_func functions
    se modifico el rebote entre las paredes pero falra desacelerar

--- Lab_2/project/MultiCollision.py
+++ Lab_2/project/MultiCollision.py
@@ -5,42 +5,68 @@ class MySphere:
     def __init__(self, pos, radius):
         self.pos = pos
         self.radius = radius    
-        self.velocity = [0,-10,0] # la esfera cae, por eso tiene una velocida hacia abajo
-        self.last_velocity = [0,-10,0]
+        self.velocity = [30,0,30] # la esfera avanza hacia el eje X Y z
+        self.last_velocity = [30,0,30]
         self.actor = None
 
 class MyFloor:
-    def __init__(self, pos, height,posWalls):
+    def __init__(self, pos, height,width,depth):
         self.pos = pos
         self.height = height    
         self.actor = None
-        self.posWalls = posWalls
+        self.width = width
+        self.depth = depth
+        self.height = height
 
-sphere = MySphere([0,2.5,0], 2)
-floor = MyFloor([0,0,0],1,20)
-time = 0
-g = 9.8
+sphere = MySphere([-10,2.5,15], 2)
+floor = MyFloor([0,0,0],1,40,40)
+time = 0.02
+aceleration = 1.80 #m/(s**2)
 #height = abs(floor.pos[1] - sphere.pos[1]) # = (1/2)*g*time^2
-
+(-1*sphere.velocity[0])#MRUV
 def set_initial_position():
     sphere_actor.SetPosition(sphere.pos[0], sphere.pos[1], sphere.pos[2])
     floor_actor.SetPosition(floor.pos[0], floor.pos[1], floor.pos[2])
-    wallXPos_actor.SetPosition(floor.posWalls-0.5,1,0)
-    wallXNeg_actor.SetPosition(-floor.posWalls+0.5,1,0)
-    wallZPos_actor.SetPosition(0,1,floor.posWalls-0.5)
-    wallZNeg_actor.SetPosition(0,1,-floor.posWalls+0.5)
+    wallXPos_actor.SetPosition((floor.width/2)-0.5,1,0)
+    wallXNeg_actor.SetPosition((-floor.width/2)+0.5,1,0)
+    wallZPos_actor.SetPosition(0,1,(floor.depth/2)-0.5)
+    wallZNeg_actor.SetPosition(0,1,(-floor.depth/2)+0.5)
 
 
 def callback_func(caller, timer_event):
-    global time
+    global aceleration,time
+    print('pos: ', sphere.pos)
+    print('aceleration: ', aceleration)
+    print('velocity: ', sphere.velocity)
+    sphere.pos[0] = sphere.pos[0] + sphere.velocity[0]*time #MRU
+    sphere.pos[2] = sphere.pos[2] + sphere.velocity[2]*time #MRU
+    
+    sphere.last_velocity[0] = sphere.velocity[0]
+    sphere.last_velocity[2] = sphere.velocity[2]
 
-    dist = g*(time**2)
-    sphere.pos[0] += dist
-    sphere.pos[2] += dist
-    sphere_actor.SetPosition(sphere.pos[0], sphere.pos[1], sphere.pos[2])           
-    time += 0.001
+    if (sphere.pos[0] + sphere.radius > (floor.width/2) - 1) or ((sphere.pos[0]) - sphere.radius < (-1*(floor.width/2)) + 1):
+        print("HOLAA MUNDO")
+        print("HOLAA MUNDO")
+        print("HOLAA MUNDO")
+        print("HOLAA MUNDO")
+        sphere.velocity[0] = (-1*sphere.velocity[0])#MRUV
+        #aceleration-=1
+    elif(sphere.pos[2] + sphere.radius > (floor.depth/2) - 1) or ((sphere.pos[2]) - sphere.radius < (-1*(floor.depth/2)) + 1):
+        sphere.velocity[2] = (-1*sphere.velocity[2]) #MRUV
+        #aceleration-=1
+    else:
+        
+        sphere.velocity[0] = sphere.velocity[0] + aceleration*time #MRUV
+        sphere.velocity[2] = sphere.velocity[2] + aceleration*time #MRUV
+      
+
+
+    sphere.actor.SetPosition(sphere.pos[0], sphere.pos[1], sphere.pos[2])
+    #time+=0.0001
+    aceleration += 0.001
     render_window.Render()
-
+    
+    
 
 # source
 ##sphere
@@ -93,6 +119,7 @@ wall_mapperZ.SetInputData(sourceWallZ.GetOutput())
 sphere_actor = vtk.vtkActor()
 sphere_actor.SetMapper(sphere_mapper)
 sphere_actor.GetProperty().SetColor(0.0, 1.0, 0.0)
+sphere.actor = sphere_actor
 
 ##floor
 floor_actor = vtk.vtkActor()
@@ -125,6 +152,13 @@ transform.Translate(0.0, 0.0, 0.0)
 axes = vtk.vtkAxesActor()
 axes.SetUserTransform(transform)
 
+
+#camera
+camera = vtk.vtkCamera()
+camera.SetFocalPoint(0,0,0)
+#camera.SetPosition(0,sphere.pos[1],sphere.pos[1]*2.7)
+camera.SetPosition(0,150,50)
+
 #renderer
 renderer = vtk.vtkRenderer()
 renderer.SetBackground(0.0, 0.0, 0.0)
@@ -135,6 +169,7 @@ renderer.AddActor(wallXNeg_actor)
 renderer.AddActor(wallZPos_actor)
 renderer.AddActor(wallZNeg_actor)
 renderer.AddActor(sphere_actor)
+renderer.SetActiveCamera(camera)
 
 
 #renderWindow
