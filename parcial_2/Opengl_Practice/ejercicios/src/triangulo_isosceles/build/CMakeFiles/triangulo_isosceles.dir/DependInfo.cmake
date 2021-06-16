b007643f85c3685c18fa4ff523d490122a2a3b97
kpzaolod6000 <kpzaolod6000@gmail.com>
2021-06-16 17:25:34 -0500

    change :+1:

---
+++ parcial_2/Opengl_Practice/ejercicios/src/triangulo_isosceles/build/CMakeFiles/triangulo_isosceles.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/judal/Documentos/COMPUTACION/Graphics-Computing/Opengl_Practice/ejercicios/src/triangulo_isosceles/dep/include/glad/glad/glad.c" "/home/judal/Documentos/COMPUTACION/Graphics-Computing/Opengl_Practice/ejercicios/src/triangulo_isosceles/build/CMakeFiles/triangulo_isosceles.dir/dep/include/glad/glad/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../dep/include/glad"
+  "../dep/include/glfw"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/judal/Documentos/COMPUTACION/Graphics-Computing/Opengl_Practice/ejercicios/src/triangulo_isosceles/main.cpp" "/home/judal/Documentos/COMPUTACION/Graphics-Computing/Opengl_Practice/ejercicios/src/triangulo_isosceles/build/CMakeFiles/triangulo_isosceles.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../dep/include/glad"
+  "../dep/include/glfw"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ parcial_2/Opengl_Practice/ejercicios/src/triangulo_isosceles/build/CMakeFiles/triangulo_isosceles.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/triangulo_isosceles.dir/dep/include/glad/glad/glad.c.o"
+  "CMakeFiles/triangulo_isosceles.dir/main.cpp.o"
+  "triangulo_isosceles"
+  "triangulo_isosceles.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/triangulo_isosceles.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
